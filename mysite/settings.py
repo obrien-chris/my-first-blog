20307c033efe71d0b2b76dcf4ca9c4412e90d70a
Chris O'Brien <subtlelotus@gmail.com>
2015-08-26 20:17:50 -0400

    Changed the HTML for the site.

--- mysite/settings.py
+++ mysite/settings.py
@@ -100,5 +100,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_URL = '/home/rwilco/python/learning/djangogirls/static/'
-STATIC_Root = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
